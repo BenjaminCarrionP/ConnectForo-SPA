354bab63749a747d9b8bb59c85c57a46d49f92cf
Felipe F. <163019974+Lionn002@users.noreply.github.com>
2025-04-28 18:11:27 -0400

    Delete foroapp/foroapp directory
    Empezando desde cero

--- foroapp/foroapp/iniciar.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d %~dp0
-
-echo Iniciando el foro...
-echo ======================
-
-mvnw.cmd spring-boot:run
-
-pause
--- foroapp/foroapp/src/main/java/com/foro/foroapp/Model/Rol.java
+++
@@ -1,7 +0,0 @@
-package com.foro.foroapp.Model;
-
-public enum Rol {
-    USUARIO,
-    MODERADOR,
-    ADMIN
-}
