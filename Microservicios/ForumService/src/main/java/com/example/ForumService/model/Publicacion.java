17edfc1d9570f8bcb04939c515d29bfc6148ee56
FUFU <benjamincarrionp@gmail.com>
2025-05-02 16:33:44 -0400

    update

--- Microservicios/ForumService/src/main/java/com/example/ForumService/model/Publicacion.java
+++ Microservicios/ForumService/src/main/java/com/example/ForumService/model/Publicacion.java
@@ -1,11 +1,19 @@
 package com.example.ForumService.model;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "publicaciones")
--- Microservicios/ForumService/src/main/java/com/example/ForumService/repository/TemaRepository.java
+++ Microservicios/ForumService/src/main/java/com/example/ForumService/repository/TemaRepository.java
@@ -1,8 +1,9 @@
 package com.example.ForumService.repository;
 
-import com.example.ForumService.model.Tema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.example.ForumService.model.Tema;
 
 @Repository
 public interface TemaRepository extends JpaRepository<Tema, Long> {
