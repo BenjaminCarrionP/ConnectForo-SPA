fc3748fb212865ab7ea869b81ae84098d5edd360
FUFU <benjamincarrionp@gmail.com>
2025-05-08 11:06:25 -0400

    Completado!
    Microservicios conectados y funcionales!

--- Microservicios/CommentService/src/main/java/com/example/CommentService/controller/ComentarioController.java
+++ Microservicios/CommentService/src/main/java/com/example/CommentService/controller/ComentarioController.java
@@ -1,12 +1,19 @@
 package com.example.CommentService.controller;
 
-import com.example.CommentService.model.Comentario;
-import com.example.CommentService.repository.ComentarioRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.example.CommentService.model.Comentario;
+import com.example.CommentService.repository.ComentarioRepository;
 
 @RestController
 @RequestMapping("/api/comentarios")
@@ -14,6 +21,11 @@ public class ComentarioController {
 
     @Autowired
     private ComentarioRepository comentarioRepository;
+
+    @GetMapping
+    public List<Comentario> getAllComentarios() {
+        return comentarioRepository.findAll();
+    }
 
     @GetMapping("/publicacion/{publicacionId}")
     public List<Comentario> getComentariosByPublicacion(@PathVariable Long publicacionId) {
--- iniciar_microservicios.bat
+++ iniciar_microservicios.bat
@@ -3,4 +3,4 @@ REM Este script inicia los microservicios de la aplicación en diferentes ventan
 start cmd /k "cd Microservicios\ForumService && mvn clean install && mvn spring-boot:run" 
 start cmd /k "cd Microservicios\CommentService && mvn clean install && mvn spring-boot:run"
 start cmd /k "cd Microservicios\NotificationService && mvn clean install && mvn spring-boot:run"
-
+start cmd /k "cd Microservicios\AnalyticsService && mvn clean install && mvn spring-boot:run"
