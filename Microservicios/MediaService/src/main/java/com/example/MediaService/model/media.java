6cdcde6e235386dd5087b6a8c84803d65fb190c3
FUFU <benjamincarrionp@gmail.com>
2025-05-26 20:38:16 -0400

    Implementacion

---
+++ Microservicios/MediaService/src/main/java/com/example/MediaService/model/media.java
@@ -0,0 +1,34 @@
+package com.example.MediaService.model;
+
+import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class Media {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String url;
+    private String tipo; // MIME: image/png, video/mp4
+    private Long tamaño;
+
+    // Asociación polimórfica
+    private Long referenciaId; // puede ser el ID del usuario o publicación
+    private String referenciaTipo; // "usuario" o "foro"
+
+    private Date fechaCreacion;
+}
+
